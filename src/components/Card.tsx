7aaf50d09e6484257bddca8f326e90fbd40dd3e6
Mike <mihaildaryin@gmail.com>
2022-07-27 22:16:39 +0300

    начинаю изучать react в связке с typescript, развернуто приложение, создан первый компонент

---
+++ src/components/Card.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface CardProps {
+  width?: string;
+  height?: string;
+  children?: React.ReactNode | React.ReactElement;
+}
+
+export const Card = ({ width, height, children }: CardProps) => {
+  return (
+    <div style={{ width, height, border: "1px solid gray" }}>{children}</div>
+  );
+};
