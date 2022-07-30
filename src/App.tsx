42bad858f7bf749f56fe640ac16de50585e258bf
Mike <mihaildaryin@gmail.com>
2022-07-30 21:35:19 +0300

    Изучена типизация props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { Card } from "./components/Card";
+import { Card, CardVariant } from "./components/Card";
 
 function App() {
   return (
     <div className="App">
-      <Card width="200px" height="200px">
+      <Card
+        onclick={(num) => console.log("click", num)}
+        variant={CardVariant.primary}
+        width="200px"
+        height="200px"
+      >
         <button>Кнопка</button>
       </Card>
     </div>
