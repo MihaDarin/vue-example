637a0249cfcd9ce51fd5b0e325a391c184e16421
Mike <mihaildaryin@gmail.com>
2022-08-02 23:31:03 +0300

    Создан переиспользуемый и типизируемый компонент для списков

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Card, CardVariant } from "./components/Card";
-import { UserList } from "./components/UserList";
-import { IUser } from "./types/types";
+import List from "./components/List";
+import TodoItem from "./components/TodoItem";
+import { UserItem } from "./components/UserItem";
+// import { UserList } from "./components/UserList";
+import { ITodo, IUser } from "./types/types";
 
 function App() {
   const [users, setUsers] = useState<IUser[]>([]);
-
+  const [todos, setTodos] = useState<ITodo[]>([]);
   useEffect(() => {
     fetchUsers();
+    fetchTodos();
   }, []);
 
   async function fetchUsers() {
@@ -17,6 +21,16 @@ function App() {
         "https://jsonplaceholder.typicode.com/users"
       );
       setUsers(response.data);
+    } catch (error) {
+      alert(error);
+    }
+  }
+  async function fetchTodos() {
+    try {
+      const response = await axios.get<ITodo[]>(
+        "https://jsonplaceholder.typicode.com/todos?_limit=10"
+      );
+      setTodos(response.data);
     } catch (error) {
       alert(error);
     }
@@ -31,7 +45,14 @@ function App() {
       >
         <button>Кнопка</button>
       </Card>
-      <UserList users={users} />
+      <List
+        items={users}
+        renderItem={(user: IUser) => <UserItem user={user} key={user.id} />}
+      />
+      <List
+        items={todos}
+        renderItem={(todo: ITodo) => <TodoItem todo={todo} key={todo.id} />}
+      />
     </div>
   );
 }
