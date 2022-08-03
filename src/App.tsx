28d06b8d7d75dd27202232948d2a12ca1f000760
Mike <mihaildaryin@gmail.com>
2022-08-03 21:43:11 +0300

    Изучена типизация событий

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Card, CardVariant } from "./components/Card";
+import EventsExample from "./components/EventsExample";
 import List from "./components/List";
 import TodoItem from "./components/TodoItem";
 import { UserItem } from "./components/UserItem";
@@ -37,6 +38,7 @@ function App() {
   }
   return (
     <div className="App">
+      <EventsExample/>
       <Card
         onclick={(num) => console.log("click", num)}
         variant={CardVariant.primary}
